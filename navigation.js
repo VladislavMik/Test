d455471415d201e68c0314c1560531992affd4c9
mikhailov.zp.network@gmail.com <mikhailov.zp.network@gmail.com>
2022-09-07 13:29:40 +0300

    added and cheked native-stack

--- navigation.js
+++ navigation.js
@@ -2,12 +2,18 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "./screens/HomeScreen";
 import SettingsScreen from "./screens/SettingsScreen";
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+
+const Stack = createNativeStackNavigator()
 
 const Navigation = () => {
     return (
         <NavigationContainer>
-            <HomeScreen />
-            <SettingsScreen />
+            <Stack.Navigator initialRouteName='Home'>
+                <Stack.Screen name='Home' component={HomeScreen} />
+                <Stack.Screen name='Settings' component={SettingsScreen} />
+            </Stack.Navigator>
         </NavigationContainer>
     )
 }
