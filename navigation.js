e47cf2397a4be607ced2a739e5c354e2f9c78fce
mikhailov.zp.network@gmail.com <mikhailov.zp.network@gmail.com>
2022-09-07 13:57:36 +0300

    add bottom navigation. Worked

--- navigation.js
+++ navigation.js
@@ -2,19 +2,19 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "./screens/HomeScreen";
 import SettingsScreen from "./screens/SettingsScreen";
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+//import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-
-const Stack = createNativeStackNavigator()
+const Tab = createBottomTabNavigator()
 
 const Navigation = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName='Home'>
-                <Stack.Screen name='Home' component={HomeScreen} />
-                <Stack.Screen name='Settings' component={SettingsScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <Tab.Navigator>
+                <Tab.Screen name='Home' component={HomeScreen} />
+                <Tab.Screen name='Settings' component={SettingsScreen} />
+            </Tab.Navigator>
+            </NavigationContainer>
     )
 }
 
