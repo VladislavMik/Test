997361d0574b444cb9403cd42a668dc13b1d4409
mikhailov.zp.network@gmail.com <mikhailov.zp.network@gmail.com>
2022-09-07 16:02:23 +0300

    add icons on bottom tab

--- navigation.js
+++ navigation.js
@@ -4,17 +4,40 @@ import HomeScreen from "./screens/HomeScreen";
 import SettingsScreen from "./screens/SettingsScreen";
 //import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
 const Tab = createBottomTabNavigator()
 
 const Navigation = () => {
     return (
         <NavigationContainer>
-        <Tab.Navigator>
-                <Tab.Screen name='Home' component={HomeScreen} />
-                <Tab.Screen name='Settings' component={SettingsScreen} />
-            </Tab.Navigator>
-            </NavigationContainer>
+            <Tab.Navigator
+      initialRouteName="Home"
+      activeColor="#e91e63"
+      barStyle={{ backgroundColor: 'tomato' }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          tabBarLabel: 'Updates',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="bell" color={color} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+        </NavigationContainer>
     )
 }
 
