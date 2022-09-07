253e71c3ab6656dd8804b7c55afde0f87608dcbf
mikhailov.zp.network@gmail.com <mikhailov.zp.network@gmail.com>
2022-09-07 13:43:17 +0300

    add moving between screens

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 
-const SettingsScreen = () => {
+
+const SettingsScreen = ({navigation}) => {
   return (
     <View>
       <Text>Hello</Text>
+      <Button title='Go to Home' onPress={() => navigation.navigate('Home')} />
     </View>
   )
 }
